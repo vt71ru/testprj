90e882a657f3b3cf0a6eaf5e2d7eeffe4103fa5e
vt71ru <69967509+vt71ru@users.noreply.github.com>
2023-07-07 11:21:00 +0300

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -155,19 +155,18 @@ echo "-------------------------------------------------"
 echo "Display and Audio Drivers"
 echo "-------------------------------------------------"
 
-gpu_type=$(lspci)
-if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
-    pacman -S --noconfirm --needed nvidia
-	nvidia-utils
-elif lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
-    pacman -S --noconfirm --needed xf86-video-amdgpu
-elif grep -E "Integrated Graphics Controller" <<< ${gpu_type}; then
-    pacman -S --noconfirm --needed libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
-elif grep -E "Intel Corporation UHD" <<< ${gpu_type}; then
-    pacman -S --needed --noconfirm libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
-fi
-pacman -S xorg pulseaudio --noconfirm --needed
-
+#gpu_type=$(lspci)
+#if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
+#    pacman -S --noconfirm --needed nvidia
+#	nvidia-utils
+#elif lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
+#    pacman -S --noconfirm --needed xf86-video-amdgpu
+#elif grep -E "Integrated Graphics Controller" <<< ${gpu_type}; then
+#    pacman -S --noconfirm --needed libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
+#elif grep -E "Intel Corporation UHD" <<< ${gpu_type}; then
+#    pacman -S --needed --noconfirm libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
+#fi
+pacman -S xorg
 systemctl enable NetworkManager bluetooth
 
 #DESKTOP ENVIRONMENT
