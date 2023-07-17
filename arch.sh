d13d55d626f3bbcec8329c993932f7443e4a649f
vt71ru <69967509+vt71ru@users.noreply.github.com>
2023-07-17 15:35:23 +0300

    Update arch.sh
    check

--- arch.sh
+++ arch.sh
@@ -21,20 +21,6 @@ if [ "$USER" != "root" ]; then
     exit 0
 fi
 
-#echo "Device list"
-#print_info "Device list: $(find /dev/ -regex "/dev/\([vs]d[a-z]\|nvme[0-9]n[0-9]\)")\n"
-# loop as long as $device is a valid device
-#while [ -z "$device" ] || [ ! -e "$device" ] || \
-#    ! expr "$device" : '^/dev/\([sv]d[a-z]\|nvme[0-9]n[0-9]\)$' >/dev/null; do
-#    print_info "Type the device name ('/dev/' required): "
-#    read -r device
-#    [ ! -e "$device" ] && print_error "This device doesn't exist\n"
-#    if ! expr "$device" : '^/dev/\([sv]d[a-z]\|nvme[0-9]n[0-9]\)$' >/dev/null; then
-#        print_error "You should type a device name, not a partition name\n"
-#        device=""
-#    fi
-#done
-
 # Ставим русскую раскладку
 echo "Setting keyboard layout..."
 loadkeys ru
@@ -50,7 +36,7 @@ export LANG=ru_RU.UTF-8
 
 up=`ping -c 1 -q raw.githubusercontent.com`
 case $? in
-0) echo -en "+ \033[32;1;49mInternet is available \033[0m\n";;
+0) echo -en "+ \033[32;1;49mInternet is available \033[0m\n"
 *) echo -en "- \033[31;1;49mWarning! \033[0m\033[31;1;5mInternet not available \033[0m\n"
    exit;;
 esac
@@ -79,9 +65,9 @@ sleep 2s
 if [[ -z ${SYSTEM_DRIVE} ]]; then
 	echo -en "+ \033[32;1;49mDevice name is incorrect \033[0m\n"
 	exit
-elif [[ "$SYSTEM_DRIVE" == "nvme0n"[1-9] ]]; then
+elif [[ "$SYSTEM_DRIVE" == *"nvme0n"[1-9] ]]; then
 	cfdisk $SYSTEM_DRIVE
-elif [[ "$disk" == "sd"[a-z] ]]; then
+elif [[ "$disk" == *"sd"[a-z] ]]; then
 	cfdisk $SYSTEM_DRIVE
 fi
 
@@ -173,102 +159,102 @@ sleep 5s
 cat <<REALEND > /mnt/next.sh
 echo "Change password to root...."
 echo root:$ROOT_PASSWORD | chpasswd
+#
+#echo "Set new user...."
+#useradd -m $USER
+#usermod -aG wheel,storage,power,audio $USER
+#echo $USER:$PASSWORD | chpasswd
+#sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
-echo "Set new user...."
-useradd -m $USER
-usermod -aG wheel,storage,power,audio $USER
-echo $USER:$PASSWORD | chpasswd
-sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+#echo "-------------------------------------------------"
+#echo "Setup Language to US and set locale"
+#echo "-------------------------------------------------"
+#sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+#sed -i 's/^#ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' /etc/locale.gen
+#locale-gen
+#echo "LANG=ru_RU.UTF-8" >> /etc/locale.conf
+#
+#ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+#hwclock --systohc
 
-echo "-------------------------------------------------"
-echo "Setup Language to US and set locale"
-echo "-------------------------------------------------"
-sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-sed -i 's/^#ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' /etc/locale.gen
-locale-gen
-echo "LANG=ru_RU.UTF-8" >> /etc/locale.conf
+#touch /etc/vconsole.conf
+#cat <<EOF > /etc/vconsole.conf
+#LOCALE="ru_RU.UTF-8"
+#KEYMAP="ru" # Или ru-mab для раскладки с переключением по Ctrl-Shift
+#FONT="cyr-sun16"
+#CONSOLEFONT="cyr-sun16" # Можно поэкспериментировать с другими шрифтами ter-v* из /usr/share/kbd/consolefonts
+#CONSOLEMAP=""
+#USECOLOR="yes"
+#EOF
 
-ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
-hwclock --systohc
+#echo "archpc" > /etc/hostname
+#cat <<EOF > /etc/hosts
+#127.0.0.1	localhost
+#::1			localhost
+#127.0.1.1	arch.localdomain	arch
+#EOF
+#echo "--------------------------------------"
+#echo "-- Edit pacman.conf                 --"
+#echo "--------------------------------------"
+#sleep 5s
+#sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf
+#sed -i 's/#[multilib]/[multilib]/g' /etc/pacman.conf
+#sed -i 's/#Include = /etc/pacman.d/mirrorlist/Include = /etc/pacman.d/mirrorlist/g'  /etc/pacman.conf
+#
+#echo "--------------------------------------"
+#echo "-- Bootloader Installation          --"
+#echo "--------------------------------------"
 
-touch /etc/vconsole.conf
-cat <<EOF > /etc/vconsole.conf
-LOCALE="ru_RU.UTF-8"
-KEYMAP="ru" # Или ru-mab для раскладки с переключением по Ctrl-Shift
-FONT="cyr-sun16"
-CONSOLEFONT="cyr-sun16" # Можно поэкспериментировать с другими шрифтами ter-v* из /usr/share/kbd/consolefonts
-CONSOLEMAP=""
-USECOLOR="yes"
-EOF
+#pacman -Syy
+#if [[ $BOOTLOADER == '1' ]]
+#then
+#pacman -S  grub  efibootmgr os-prober --noconfirm --needed
+#sleep 5s
+#grub-install --target=x86_64-efi --efi-directory=/boot    --bootloader-id=ARCH
+#sleep 5s
+#sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/g' /etc/default/grub
+#grub-mkconfig -o /boot/grub/grub.cfg
+#sleep 5s
+#else [[ $BOOTLOADER == '2' ]]
+#then
+#bootctl install --path /mnt/boot
+#echo "default arch.conf" >> /mnt/boot/loader/loader.conf
+#cat <<EOF > /mnt/boot/loader/entries/arch.conf
+#title Arch Linux
+#linux /vmlinuz-linux
+#initrd /initramfs-linux.img
+#options root=${ROOT} rw
+#EOF
+#fi
 
-echo "archpc" > /etc/hostname
-cat <<EOF > /etc/hosts
-127.0.0.1	localhost
-::1			localhost
-127.0.1.1	arch.localdomain	arch
-EOF
-echo "--------------------------------------"
-echo "-- Edit pacman.conf                 --"
-echo "--------------------------------------"
-sleep 5s
-sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf
-sed -i 's/#[multilib]/[multilib]/g' /etc/pacman.conf
-sed -i 's/#Include = /etc/pacman.d/mirrorlist/Include = /etc/pacman.d/mirrorlist/g'  /etc/pacman.conf
+#echo "-------------------------------------------------"
+#echo -en "+ \033[32;1;49mDisplay and audio drivers \033[0m\n"
+#echo "-------------------------------------------------"
 
-echo "--------------------------------------"
-echo "-- Bootloader Installation          --"
-echo "--------------------------------------"
-
-pacman -Syy
-if [[ $BOOTLOADER == '1' ]]
-then
-pacman -S  grub  efibootmgr os-prober --noconfirm --needed
-sleep 5s
-grub-install --target=x86_64-efi --efi-directory=/boot    --bootloader-id=ARCH
-sleep 5s
-sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/g' /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
-sleep 5s
-else [[ $BOOTLOADER == '2' ]]
-then
-bootctl install --path /mnt/boot
-echo "default arch.conf" >> /mnt/boot/loader/loader.conf
-cat <<EOF > /mnt/boot/loader/entries/arch.conf
-title Arch Linux
-linux /vmlinuz-linux
-initrd /initramfs-linux.img
-options root=${ROOT} rw
-EOF
-fi
-
-echo "-------------------------------------------------"
-echo -en "+ \033[32;1;49mDisplay and audio drivers \033[0m\n"
-echo "-------------------------------------------------"
-
-pacman -S xorg
-systemctl enable NetworkManager bluetooth
+#pacman -S xorg
+#systemctl enable NetworkManager bluetooth
 
 #DESKTOP ENVIRONMENT
-if [[ $DESKTOP == '1' ]]
-then 
-    pacman -S gnome gdm --noconfirm --needed
-    systemctl enable gdm
-elif [[ $DESKTOP == '2' ]]
-then
-    #pacman -S plasma sddm kde-applications --noconfirm --needed
-    pacman -S plasma sddm --noconfirm --needed
-    systemctl enable sddm
-elif [[ $DESKTOP == '3' ]]
-then
-    pacman -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter --noconfirm --needed
-    systemctl enable lightdm
-else
-    echo "You have choosen to Install Desktop Yourself"
-fi
+#if [[ $DESKTOP == '1' ]]
+#then 
+#    pacman -S gnome gdm --noconfirm --needed
+#    systemctl enable gdm
+#elif [[ $DESKTOP == '2' ]]
+#then
+#    #pacman -S plasma sddm kde-applications --noconfirm --needed
+#    pacman -S plasma sddm --noconfirm --needed
+#    systemctl enable sddm
+#elif [[ $DESKTOP == '3' ]]
+#then
+#    pacman -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter --noconfirm --needed
+#    systemctl enable lightdm
+#else
+#    echo "You have choosen to Install Desktop Yourself"
+#fi
 
-echo "-------------------------------------------------"
-echo "Install Complete, You can reboot now"
-echo "-------------------------------------------------"
+#echo "-------------------------------------------------"
+#echo "Install Complete, You can reboot now"
+#echo "-------------------------------------------------"
 
 REALEND
 
