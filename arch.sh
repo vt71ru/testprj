dad71bd9349bd64195a188087b8024fbff1c24b9
vt71ru <69967509+vt71ru@users.noreply.github.com>
2023-07-19 16:08:32 +0300

    Update arch.sh
    Check some error

--- arch.sh
+++ arch.sh
@@ -37,6 +37,7 @@ export LANG=ru_RU.UTF-8
 up=`ping -c 1 -q raw.githubusercontent.com`
 case $? in
 0) echo -en "+ \033[32;1;49mInternet is available \033[0m\n"
+	;;
 *) echo -en "- \033[31;1;49mWarning! \033[0m\033[31;1;5mInternet not available \033[0m\n"
    exit;;
 esac
@@ -130,7 +131,7 @@ echo -en "+ \033[32;1;49mCreate Root partition \033[0m\n"
 mkfs.ext4 -L "ROOT" "${ROOT}"
 
 # mount target
-echo -en "+ \033[32;1;49mMount some partition \033[0m\n";;
+echo -en "+ \033[32;1;49mMount some partition \033[0m\n"
 mount -t ext4 "${ROOT}" /mnt
 mkdir /mnt/boot
 mount -t vfat "${EFI}" /mnt/boot/
@@ -159,105 +160,103 @@ sleep 5s
 cat <<REALEND > /mnt/next.sh
 echo "Change password to root...."
 echo root:$ROOT_PASSWORD | chpasswd
-#
-#echo "Set new user...."
-#useradd -m $USER
-#usermod -aG wheel,storage,power,audio $USER
-#echo $USER:$PASSWORD | chpasswd
-#sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+echo "Set new user...."
+useradd -m $USER
+usermod -aG wheel,storage,power,audio $USER
+echo $USER:$PASSWORD | chpasswd
+sudo sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
-#echo "-------------------------------------------------"
-#echo "Setup Language to US and set locale"
-#echo "-------------------------------------------------"
-#sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-#sed -i 's/^#ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' /etc/locale.gen
-#locale-gen
-#echo "LANG=ru_RU.UTF-8" >> /etc/locale.conf
+echo "-------------------------------------------------"
+echo "Setup Language to US and set locale"
+echo "-------------------------------------------------"
+sudo sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sudo sed -i 's/^#ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' /etc/locale.gen
+sudo locale-gen
+sudo echo "LANG=ru_RU.UTF-8" >> /etc/locale.conf
 #
-#ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
-#hwclock --systohc
+sudo ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+sudo hwclock --systohc
 
-#touch /etc/vconsole.conf
-#cat <<EOF > /etc/vconsole.conf
-#LOCALE="ru_RU.UTF-8"
-#KEYMAP="ru" # Или ru-mab для раскладки с переключением по Ctrl-Shift
-#FONT="cyr-sun16"
+sudo touch /etc/vconsole.conf
+sudo cat <<EOF > /etc/vconsole.conf
+LOCALE="ru_RU.UTF-8"
+KEYMAP="ru" # Или ru-mab для раскладки с переключением по Ctrl-Shift
+FONT="cyr-sun16"
 #CONSOLEFONT="cyr-sun16" # Можно поэкспериментировать с другими шрифтами ter-v* из /usr/share/kbd/consolefonts
 #CONSOLEMAP=""
 #USECOLOR="yes"
-#EOF
+EOF
 
-#echo "archpc" > /etc/hostname
-#cat <<EOF > /etc/hosts
-#127.0.0.1	localhost
-#::1			localhost
-#127.0.1.1	arch.localdomain	arch
-#EOF
-#echo "--------------------------------------"
-#echo "-- Edit pacman.conf                 --"
-#echo "--------------------------------------"
-#sleep 5s
-#sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf
-#sed -i 's/#[multilib]/[multilib]/g' /etc/pacman.conf
-#sed -i 's/#Include = /etc/pacman.d/mirrorlist/Include = /etc/pacman.d/mirrorlist/g'  /etc/pacman.conf
+echo "archpc" > /etc/hostname
+sudo cat <<EOF > /etc/hosts
+127.0.0.1	localhost
+::1			localhost
+127.0.1.1	arch.localdomain	arch
+EOF
+echo "--------------------------------------"
+echo "-- Edit pacman.conf                 --"
+echo "--------------------------------------"
+sleep 5s
+sudo sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf
+sudo sed -i 's/#[multilib]/[multilib]/g' /etc/pacman.conf
+sudo sed -i 's/#Include = /etc/pacman.d/mirrorlist/Include = /etc/pacman.d/mirrorlist/g'  /etc/pacman.conf
 #
-#echo "--------------------------------------"
-#echo "-- Bootloader Installation          --"
-#echo "--------------------------------------"
+echo "--------------------------------------"
+echo "-- Bootloader Installation          --"
+echo "--------------------------------------"
 
-#pacman -Syy
-#if [[ $BOOTLOADER == '1' ]]
-#then
-#pacman -S  grub  efibootmgr os-prober --noconfirm --needed
-#sleep 5s
-#grub-install --target=x86_64-efi --efi-directory=/boot    --bootloader-id=ARCH
-#sleep 5s
-#sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/g' /etc/default/grub
-#grub-mkconfig -o /boot/grub/grub.cfg
-#sleep 5s
-#else [[ $BOOTLOADER == '2' ]]
-#then
-#bootctl install --path /mnt/boot
-#echo "default arch.conf" >> /mnt/boot/loader/loader.conf
-#cat <<EOF > /mnt/boot/loader/entries/arch.conf
-#title Arch Linux
-#linux /vmlinuz-linux
-#initrd /initramfs-linux.img
-#options root=${ROOT} rw
-#EOF
-#fi
+sudo pacman -Syy
+if [[ $BOOTLOADER == '1' ]]
+then
+sudo pacman -S  grub  efibootmgr os-prober --noconfirm --needed
+sleep 5s
+sudo grub-install --target=x86_64-efi --efi-directory=/boot    --bootloader-id=ARCH
+sleep 5s
+sudo sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/g' /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+sleep 5s
+else [[ $BOOTLOADER == '2' ]]
+then
+bootctl install --path /mnt/boot
+echo "default arch.conf" >> /mnt/boot/loader/loader.conf
+cat <<EOF > /mnt/boot/loader/entries/arch.conf
+title Arch Linux
+linux /vmlinuz-linux
+initrd /initramfs-linux.img
+options root=${ROOT} rw
+EOF
+fi
 
 #echo "-------------------------------------------------"
 #echo -en "+ \033[32;1;49mDisplay and audio drivers \033[0m\n"
 #echo "-------------------------------------------------"
 
-#pacman -S xorg
-#systemctl enable NetworkManager bluetooth
+sudo pacman -S xorg
+sudo systemctl enable NetworkManager bluetooth
 
 #DESKTOP ENVIRONMENT
-#if [[ $DESKTOP == '1' ]]
-#then 
-#    pacman -S gnome gdm --noconfirm --needed
-#    systemctl enable gdm
-#elif [[ $DESKTOP == '2' ]]
-#then
-#    #pacman -S plasma sddm kde-applications --noconfirm --needed
+if [[ $DESKTOP == '1' ]]
+then 
+    sudo pacman -S gnome gdm --noconfirm --needed
+    sudo systemctl enable gdm
+elif [[ $DESKTOP == '2' ]]
+then
+    sudo pacman -S plasma sddm dolphin --noconfirm --needed
 #    pacman -S plasma sddm --noconfirm --needed
-#    systemctl enable sddm
-#elif [[ $DESKTOP == '3' ]]
-#then
-#    pacman -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter --noconfirm --needed
-#    systemctl enable lightdm
-#else
-#    echo "You have choosen to Install Desktop Yourself"
-#fi
+    sudo systemctl enable sddm
+elif [[ $DESKTOP == '3' ]]
+then
+    pacman -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter --noconfirm --needed
+    systemctl enable lightdm
+else
+    echo "You have choosen to Install Desktop Yourself"
+fi
 
 #echo "-------------------------------------------------"
 #echo "Install Complete, You can reboot now"
 #echo "-------------------------------------------------"
 
 REALEND
-
 
 arch-chroot /mnt sh next.sh
 }
