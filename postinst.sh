773f8080c95f84c8ba0c482b738a067c6dad9ae7
vt71ru <69967509+vt71ru@users.noreply.github.com>
2023-07-31 15:07:49 +0300

    Update postinst.sh

--- postinst.sh
+++ postinst.sh
@@ -3,6 +3,15 @@ sudo pacman -Suy
 #Install ntfs support
 sudo pacman -S ntfs-3g
 
+#install yay
+mkdir /home/user/tmp
+cd /home/user/tmp
+git clone https://aur.archlinux.org/yay.git
+cd yay/
+makepkg -si
+cd ..
+sudo rm -dR yay/
+
 #install yaourt
 mkdir /home/user/tmp
 cd /home/user/tmp
@@ -16,14 +25,19 @@ makepkg -si
 cd ..
 sudo rm -dR yaourt/ package-query/
 
+pacman -S xf86-video-nouveau optimus-manager --no-needed --confirm
+git clone https://github.com/sh377c0d3/nvidia_proprietary_linux.git
+sudo pacman -S whiptail
+cd nvidia_proprietary_linux/
+chmod  +x install.sh
+./install.sh
+
 vim /etc/pacman.conf 
 #Add ILoveCandy to end of file
 
 # Install Another auto nice daemon, with community rules support
 yaourt -Ss ananicy
 sudo systemctl enable ananicy
-
-pacman -S nvidia-dkms nvidia-settings xf86-video-nouveau optimus-manager
 
 sudo systemctl enable optimus-manager.service
 sudo systemctl start optimus-manager.service
